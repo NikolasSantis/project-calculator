daafb8d7f683278f9d17d06dcba10e00391f9ddd
NikolasSantis <nicolastigosan@gmail.com>
2024-10-24 11:35:16 -0300

    feat: add-jwt_tokens-configs

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('me', function (Request $request) {
-    return ['oi' => 'teste'];
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::get('me', 'App\Http\Controllers\AuthController@me');
 });
